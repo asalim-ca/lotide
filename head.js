fd62631399d176d0c0223876b3965be493f06cf3
Ali Salim <alisalim.ca@gmail.com>
2021-05-25 00:22:57 +0000

    head() implementation / no test code!

---
+++ head.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  console.log(`${actual === expected ? '✅✅✅' : '🛑🛑🛑'} Assertion ${actual === expected ? 'Passed' : 'Failed'}: ${actual} ${actual === expected ? '=' : '!'}== ${expected}`);
+};
+
+const head = function(data) {
+  return data[0];
+}
