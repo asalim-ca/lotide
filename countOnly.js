1f74db451b8bc33c91b17280bd583d58224dc639
Ali Salim <alisalim.ca@gmail.com>
2021-05-26 19:20:30 +0000

    Another implementation without comment - I have a problem!

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  console.log(`${actual === expected ? '✅✅✅' : '🛑🛑🛑'} Assertion ${actual === expected ? 'Passed' : 'Failed'}: ${actual} ${actual === expected ? '=' : '!'}== ${expected}`);
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let count = {};
+  Object.keys(itemsToCount).forEach(item => {
+    if (itemsToCount[item] ) {
+      let occurence = countOccurence(item, allItems);
+      if (occurence) count[item] = occurence;
+    }
+  });
+  return count;
+}
+
+const countOccurence = function(item, arr) {
+  let count = 0;
+  for (const elm of arr) 
+    count += elm === item;
+  return count;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
