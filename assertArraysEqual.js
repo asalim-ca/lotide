9ce8ea82a126fea7181c2d9642715b5511d766d5
Ali Salim <alisalim.ca@gmail.com>
2021-05-31 18:06:47 +0000

    Refactor assertArraysEqual .

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,8 @@
-const eqArrays = (arr1, arr2) => {
-  let sameLength = arr1.length === arr2.length;
-  let index = 0;
-  if (sameLength)
-    while (index < arr1.length && arr1[index] === arr2[index])
-      index++;
-  return sameLength && index === arr1.length;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   let perfectMatch = eqArrays(actual, expected);
   console.log(`${perfectMatch? '✅✅✅' : '🛑🛑🛑'} Assertion ${perfectMatch ? 'Passed' : 'Failed'}: ${actual} ${perfectMatch ? '=' : '!'}== ${expected}`);
 };
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", 2, "3"]);
+module.exports = assertArraysEqual;
