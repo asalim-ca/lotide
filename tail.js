02ebb7f6de3ed86b78aad39664a186f40b87d06b
Ali Salim <alisalim.ca@gmail.com>
2021-05-31 17:55:14 +0000

    Refactor tail.

--- tail.js
+++ tail.js
@@ -1,7 +1,5 @@
-const assertEqual = function(actual, expected) {
-  console.log(`${actual === expected ? '✅✅✅' : '🛑🛑🛑'} Assertion ${actual === expected ? 'Passed' : 'Failed'}: ${actual} ${actual === expected ? '=' : '!'}== ${expected}`);
-};
-
 const tail = function(data) {
   return data.slice(1);
 };
+
+module.exports = tail;
