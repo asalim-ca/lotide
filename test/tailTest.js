3193b91ec0f604bcc05e685740d2950b28216b0e
Ali Salim <alisalim.ca@gmail.com>
2021-05-31 18:53:33 +0000

    Refactoring tail with M&CH

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert
 const tail = require('../tail');
 
-// TEST CODE
+// TEST CODE (Using Mocha and Chai)
+
+describe('#tail', () =>{
+  it("tail of [1, 2, 3, 4, 5] should be [2, 3, 4, 5]", ()=> {
+    const inputArray = [1, 2, 3, 4, 5];
+    const expectedArray = [2, 3, 4, 5];
+    assert.deepEqual(tail(inputArray), expectedArray)
+  });
+});
