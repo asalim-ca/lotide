336da81fb6133041dc8d6626d25061e2cdaf8cca
Ali Salim <alisalim.ca@gmail.com>
2021-05-31 19:09:17 +0000

    Refactored middle wit M&CH

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe('#middle', () =>{
+  it("middle of [1, 2, 3, 4, 5] should be [3]", () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const expectedArray = [3];
+    assert.deepEqual(middle(inputArray), expectedArray)
+  });
+
+  it("middle of [1, 2, 3, 4, 5, 6] should be [3, 4]", () => {
+    const inputArray = [1, 2, 3, 4, 5, 6];
+    const expectedArray = [3, 4];
+    assert.deepEqual(middle(inputArray), expectedArray)
+  });
+});
 // TEST CODE
